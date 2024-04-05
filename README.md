2392ca2e7dcd135861bcd6cb7c72d5ed8719183e
sandeepthalapanane <ssandy086@gmail.com>
2024-04-05 02:20:02 -0400

    +add files

--- README.md
+++ README.md
@@ -1,47 +1,126 @@
-![Netlify examples](netlify-badge-examples.png)
+<h1 align="center"> Personal Portfolio </h1>
 
-# Netlify Feature Tour
+<img width="945" alt="image" src="https://user-images.githubusercontent.com/76661350/193249886-79f3d04a-47c0-4b4b-93a6-23c32cf762f1.png">
 
-**Access this demo site**: https://feature-tour.netlify.app
+### Deployed link: https://parthmittal.netlify.app/
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/fad6792e-1c44-44db-bd79-ea74b42b0f89/deploy-status)](https://app.netlify.com/sites/feature-tour/deploys)
+## Table of Contents 📁
 
-## About this example site
+1. [Tech Stack](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#tech-stack)
+2. [Implemented Sections](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#implemented-sections)
+3. [Use as a theme](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#use-as-a-theme)
+4. [Contributing](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#contributing)
+5. [Installation Guide](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#installation-guide)
+6. [Sample Git Workflow](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#sample-git-workflow)
+7. [References & Inspirations](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#references--inspirations)
+8. [Illustrations](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#illustrations)
+   <br>
 
-This site provides a path to get started learning about Netlify features.
+## Tech Stack 🧰
 
-- 📚 [Docs Getting Started Tutorial](https://docs.netlify.com/get-started/?utm_medium=social&utm_source=github&utm_campaign=devex-ph&utm_content=devex-examples)
+<li>Frameworks</li>
 
-## Speedily deploy your own version
+- [ReactJS](https://reactjs.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
 
-Deploy your own version of this example site by selecting the Deploy to Netlify Button below. This will automatically:
+<li>Libraries/Tools</li>
+    
+- [ViteJS](https://vitejs.dev/)
+- [React Icons](https://react-icons.github.io/react-icons")
+- [Framer](https://www.framer.com/)
+- [React Lottie](https://www.npmjs.com/package/react-lottie)
+- [Meraki UI](https://merakiui.com/components/)
 
-- Clone a copy of this repo to your own GitHub account
-- Create a new project in your [Netlify account](https://app.netlify.com/?utm_medium=social&utm_source=github&utm_campaign=devex&utm_content=devex-examples), linked to your new repo
-- Create an automated deployment pipeline to watch for changes on your repo
-- Build and deploy your new site
+<br/>
 
-[![Deploy To Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/netlify/netlify-feature-tour&utm_medium=social&utm_source=github&utm_campaign=devex&utm_content=devex-examples)
+## Implemented Sections ☑️
 
-## Install and run this example locally
+- Hero Section
+- Skills & Experience
+- Education
+- Projects
+- Open Source
+- Extra Curricular
+- Contact Me
 
-You can clone this example repo to explore its features and implementation and to run it locally.
+## Using as a theme ✨
 
-```shell
+### Code changes
 
-# 1. Clone the repository to your local development environment
-git clone git@github.com:netlify/feature-tour.git
+Three main things have to be changed to customize it your way (please open an issue if you find more such instances):
 
-# 2. Move into the project directory
-cd feature-tour
+1. Personal Information
 
-# 3. Install code dependencies
-npm install
+- [/src/constants/index.js](https://github.com/mittal-parth/personal-portfolio/blob/main/src/constants/index.js) contains all the personal information one needs to change. Each website section is written as a JavaScript object and is pretty intuitive to change.
 
-# 4. Install the Netlify CLI to let you locally serve your site using Netlify's features
-npm install -g netlify-cli
+- Icons
+  - Whenever you want to use an icon, you'll have to make sure that the icon is imported.
+  - Head to [https://react-icons.github.io/react-icons/search](https://react-icons.github.io/react-icons/search) and search for the desired icon. (Eg: SiReact for ReactJS)
+  - Note the package it belongs to (Eg: 'Si' here)
+  - Import the icon into [`/src/constants/index.js`](https://github.com/mittal-parth/personal-portfolio/blob/main/src/constants/index.js) (Eg: `import { ... SiReact, } from "react-icons/si";` here)
 
-# 5. Serve your site using Netlify Dev
-netlify dev
+2. Website title and icon
 
-```
+- Go to [`index.html`](https://github.com/mittal-parth/personal-portfolio/blob/main/index.html) and change the [`title`](https://github.com/mittal-parth/personal-portfolio/blob/main/index.html#L7") to your name.
+- Also, change the link to the title [icon](https://github.com/mittal-parth/personal-portfolio/blob/main/index.html#L5)
+
+3. Assets
+
+- Add any assets (images) to the [`assets`](https://github.com/mittal-parth/personal-portfolio/tree/main/src/assets) folder.
+- Import the asset and export it using the[`/src/assets/index.js`](https://github.com/mittal-parth/personal-portfolio/blob/main/src/assets/index.js) file.
+
+### Deployment
+
+You can use [Netlify](https://docs.netlify.com/) to deploy your site. Follow the instructions in their docs to do so.
+
+## Contributing 🏆
+
+We welcome contributions in the form of pull requests, issues and documentation. Feel free to help us in any way! ❤️
+
+- Please read and abide by our [Code of Conduct](https://github.com/mittal-parth/personal-portfolio/blob/main/CODE_OF_CONDUCT.md);
+our community aspires to be a respectful place both during online and in-person interactions.
+- Please follow the [installation guide](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#installation-guide) and the [sample git workflow](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#sample-git-workflow) to contribute.
+
+## Installation Guide 🧑‍💻
+
+### Using Git and Github
+
+- [Fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo) the repo
+- [Clone](https://docs.github.com/en/get-started/quickstart/contributing-to-projects#cloning-a-fork) the forked repository
+- Enter the new `portfolio` directory with `cd portfolio`
+- Set the upstream remote to the original repository url so that git knows where to fetch updates from in future: `git remote add upstream https://github.com/mittal-parth/personal-portfolio.git`
+
+### Install required packages
+
+- `npm install`
+
+### Run server
+
+- `npm run dev`
+
+<br/>
+
+## Sample Git Workflow
+
+- Follow the [installation guide](https://github.com/mittal-parth/personal-portfolio/blob/main/readme.md#installation-guide) to install the software
+- Create a new feature branch with `git checkout -b <name-of-your-feature-branch>`
+- Make changes and commit them in the feature branch.
+- Once done developing, switch back to the main branch with `git checkout main` ; pull the latest version of the repo with `git pull https://github.com/mittal-parth/personal-portfolio.git main`
+- Switch back to the feature branch with `git checkout <name-of-your-feature-branch>`. Apply the new changes on top of the latest version of the repo with `git rebase main`
+- [Resolve merge conflicts](https://help.github.com/articles/resolving-a-merge-conflict-from-the-command-line/) (if any)
+- Push your feature branch upto your remote repo with `git push origin <name-of-your-feature-branch>`
+- [Submit a Pull Request](https://docs.github.com/en/get-started/quickstart/contributing-to-projects#making-a-pull-request) to the main branch.
+- After any questions or changes have been resolved, your contribution would be merged in!
+
+#### If you found this repo helpful in anyway, considering giving it a star - it would mean the world to me! 🌟
+
+## References & Inspirations 👏
+
+- [JavaScript Mastery](https://youtu.be/_oO4Qi5aVZs)
+- [Developerfolio](https://developerfolio.js.org/)
+- [MasterPortfolio](https://github.com/ashutosh1919/masterPortfolio)
+
+## Illustrations 🖼️
+
+- [Coding Lottie](https://lottiefiles.com/90189-coding) by Yamesh Sai Balaji
+- [Quiz Mode Lottie](https://lottiefiles.com/92377-quiz-mode) by SenecaDan
---
+++ src/constants/index.js
@@ -0,0 +1,688 @@
+import {
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  nitk,
+  cluboard,
+  cash_flow,
+  freadom,
+  bank,
+  gdsc,
+  iris,
+  ecell,
+  genesis,
+  graphql,
+  portfolio,
+  publiclab,
+  zulip,
+  anna,
+  cdc,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust
+} from "../assets";
+
+import { Matlab } from '../components/icons';
+
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillMail,
+  AiOutlineTwitter,
+  AiFillHtml5,
+  AiOutlineGoogle,
+  AiOutlineGitlab,
+} from "react-icons/ai";
+
+import {
+  SiDjango,
+  SiSqlite,
+  SiJavascript,
+  SiBootstrap,
+  SiReact,
+  SiTailwindcss,
+  SiGraphql,
+  SiPython,
+  SiCplusplus,
+  SiC,
+  SiRubyonrails,
+  SiJquery,
+  SiVisualstudiocode,
+  SiPostman,
+  SiGit,
+  SiOpencv,
+  SiMysql,
+  SiSolidity,
+  SiNetlify,
+  SiChartdotjs,
+  SiVite,
+  SiArduino,
+  SiWeb3Dotjs,
+  SiIpfs,
+  SiDotnet,
+  SiTwilio,
+  SiFlutter,
+  SiReplit,
+  SiCsharp,
+  SiFlask,
+  SiFigma,
+  SiGooglemaps,
+  SiOpenai,
+  SiGooglecloud,
+  SiNextdotjs,
+  SiPytorch,
+  SiTensorflow,
+  SiKeras,
+  SiScikitlearn, 
+  SiPandas,
+  SiVisualstudio,
+  SiGamedeveloper,
+  SiGoogle,
+  SiRos,
+  SiGatsby,
+  SiPointy,
+  SiDropbox,
+} from "react-icons/si";
+
+import {
+  FaHardHat
+} from "react-icons/fa";
+
+import {
+  IoIosNotificationsOutline
+} from "react-icons/io";
+
+import { DiCss3, DiJava, DiMsqlServer, DiRuby } from "react-icons/di";
+
+export const resumeLink = "https://drive.google.com/file/d/1sTKYgZfLFkEzR8MVsoZlVyw90Unov4gN/view?usp=sharing";
+export const repoLink = "https://github.com/mittal-parth/personal-portfolio";
+
+export const callToAction = "https://www.linkedin.com/in/sandeep-thalapanane/";
+
+
+export const navLinks = [
+  {
+    id: "skills",
+    title: "Skills & Experience",
+  },
+  {
+    id: "education",
+    title: "Education",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  // {
+  //   id: "openSource",
+  //   title: "Open Source",
+  // },
+  {
+    id: "extraCurricular",
+    title: "Research Experience",
+  },
+  {
+    id: "contactMe",
+    title: "Contact Me",
+  },
+];
+
+export const educationList = [
+  {
+    id: "education-1",
+    icon: nitk,
+    title: "University of Maryland, College Park, USA",
+    degree: "Master of Engineering",
+    duration: "August 2022 - May 2024",
+    content1: "Branch: Robotics",
+    content2: "CGPA: 3.96/4",
+  },
+  {
+    id: "education-2",
+    icon: anna,
+    title: "CEG, Anna University, Chennai",
+    degree: "Bachelor of Engineering",
+    duration: "August 2016 - October 2020",
+    content1: "Branch: Mechanical",
+    content2: "CGPA: 8.09/10",
+ },
+];
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    items: [
+      {
+        id: "pl-1",
+        icon: SiCplusplus,
+        name: "C++",
+      },
+      {
+        id: "pl-2",
+        icon: SiPython,
+        name: "Python",
+      },
+      {
+        id: "pl-3",
+        icon: SiCsharp, 
+        name: "C#",
+      },
+    //   {
+    //     id: "pl-4", // Unique identifier for Matlab
+    //     icon: Matlab, // Use the imported Matlab icon
+    //     name: "Matlab", // Correct name to "Matlab"
+    //  },
+    
+    ],
+  },
+  {
+    title: "Frameworks/Libraries",
+    items: [
+      {
+        id: "fl-1",
+        icon: SiOpencv, // OpenCV icon
+        name: "OpenCV",
+      },
+      {
+        id: "fl-2",
+        icon: SiPytorch, // PyTorch icon
+        name: "PyTorch",
+      },
+      {
+        id: "fl-3",
+        icon: SiTensorflow, // TensorFlow icon
+        name: "TensorFlow",
+      },
+      {
+        id: "fl-4",
+        icon: SiKeras, // Keras icon
+        name: "Keras",
+      },
+      {
+        id: "fl-5",
+        icon: SiScikitlearn, // scikit-learn icon
+        name: "scikit-learn",
+      },
+
+      {
+        id: "fl-6",
+        icon: SiPandas, // Pandas icon
+        name: "Pandas",
+      },
+       {
+        id: "fl-7",
+        icon: SiPytorch, // Placeholder icon for Pytorch3D
+        name: "Pytorch3D",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    items: [
+      {
+        id: "t-3",
+        icon: SiVisualstudiocode,
+        name: "VS Code",
+      },
+      {
+        id: "t-4",
+        icon: SiGit,
+        name: "Git",
+      },
+      {
+        id: "t-5",
+        icon: AiFillGithub,
+        name: "GitHub",
+      },
+      {
+        id: "t-6",
+        icon: AiOutlineGitlab,
+        name: "Gitlab",
+      },
+      {
+        id: "t-7",
+        icon: SiVisualstudio,
+        name: "Visual Studio",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    organisation: "TRC Robotics",
+    logo: oracle,
+    link: "",
+    positions: [
+      {
+        title: "Robotics Software Engineer Intern",
+        duration: "May 2023 - Aug 2023",
+        content: [
+          {
+            text: "Spearheaded the development of a user-friendly Grafana plug-in to control the robot and enhanced it by incorporating ROS lib JavaScript & Node JS for the robot to perform tasks & simulations on a web page, resulting in a 40% reduction in manual task time.",
+            link: "",
+          },
+          {
+            text: "Architected & customized Docker containers to integrate the Grafana plug-in enhancing user experience & streamlining workflows.",
+            link: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    organisation: "Flextronics",
+    logo: iris,
+    link: "https://flex.com/",
+    positions: [
+      {
+        title: "Senior Analyst - Procurement",
+        duration: "Apr 2021 - Aug 2022",
+        content: [
+          {
+            text: "Resolved backlog of $3 million worth of purchase orders during COVID critical times by strategically sourcing shipping partners.",
+            link: "",
+          },
+          {
+            text: "Streamlined coordination and collaboration with vendors and suppliers from all parts of the world through swift & constant communication, and achieved inventory target of $1 million.",
+            link: "",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const openSourceContributions = [
+
+];
+
+export const projects = [
+  {
+    id: "project-1",
+    title: "3D Inspection Using NeuS",
+    github: "",
+    link: "",
+    image: comicify_ai,
+    content:
+      "Formulated a 3D NeRF inspection pipeline based on HF-NeuS (SDF) & Deep Crack for 3D reconstruction of surface & crack segmentation, improved HF-NeuS with two additional losses to reduce training time.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-2",
+        icon: SiPytorch,
+        name: "Pytorch3d"
+      },
+      {
+        id: "icon-3",
+        icon: SiPytorch,
+        name: "Pytorch"
+      },
+      {
+        id: "icon-4",
+        icon: SiOpencv,
+        name: "OpenCV"
+      },
+    ],
+  },
+  {
+    id: "project-2",
+    title: "Robot Motion Planning in Learned Latent Spaces",
+    github: "https://github.com/sandeepthalapanane/Underwater-Image-Restoration",
+    link: "",
+    image: greentrust,
+    content:
+      "Implemented Learned Latent Rapidly-Exploring Random Trees to expertly address complex robot motion planning challenges by mapping to a lower-dimensional latent space by using a neural net, resulting in faster solving of high DOF motion planning.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-2",
+        icon: SiTensorflow,
+        name: "Tensorflow"
+      },
+    ],
+  },
+  {
+    id: "project-3",
+    title: "Underwater image restoration",
+    github: "https://github.com/sandeepthalapanane/Underwater-Image-Restoration",
+    link: "",
+    image: chargeswap,
+    content:
+      "Enhanced the underwater image visibility by restoring true colors using an innovative image processing pipeline. Leveraged a physics-based approach and a deep learning neural network model to estimate depth and counteract light scattering caused by water.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-2",
+        icon: SiTensorflow,
+        name: "Tensorflow"
+      },
+      {
+        id: "icon-3",
+        icon: SiOpencv,
+        name: "OpenCV"
+      },
+      {
+        id: "icon-4",
+        icon: SiScikitlearn,
+        name: "scikit-image"
+      },
+      {
+        id: "icon-5",
+        icon: SiPandas,
+        name: "Pandas"
+      },
+    ],
+  },
+  {
+    id: "project-4",
+    title: "Virtual car driving using hand gestures",
+    github: "https://github.com/sandeepthalapanane/Virtual-car-driving-system",
+    link: "",
+    image: placeicon,
+    content:
+      "Inspired by NVIDIA’s virtual driving initiative, constructed a remote-controlled automotive system by training a machine learning model & translating hand gestures into driving commands for a robot",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-2",
+        icon: SiTensorflow,
+        name: "Tensorflow"
+      },
+      {
+        id: "icon-3",
+        icon: SiOpencv,
+        name: "OpenCV"
+      },
+      {
+        id: "icon-4",
+        icon: SiRos,
+        name: "ROS 2"
+      },
+      {
+        id: "icon-5",
+        icon: SiGoogle,
+        name: "Google's Mediapipe"
+      },
+    ],
+  },
+  {
+    id: "project-5",
+    title: "Turtle Bot Path planning using A*",
+    github: "https://github.com/sandeepthalapanane/Implementation-of-A-star-algorithm-for-path-planning-of-Turtlebot-in-an-obstacle-environment",
+    link: "",
+    image: huntly,
+    content:
+      "Deployed A* algorithm, SLAM, and object detection into the TurtleBot using camera and lidar sensor and leveraging the ROS2 framework, Gazebo, and RViz to simulate robot movement in dynamic environments efficiently.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiRos,
+        name: "ROS 2"
+      },
+      {
+        id: "icon-2",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-3",
+        icon: SiGatsby,
+        name: "Gazebo"
+      },
+      {
+        id: "icon-3",
+        icon: SiGatsby,
+        name: "RViz"
+      },
+    ],
+  },
+  {
+    id: "project-6",
+    title: "Wall following Algorithm",
+    github: "https://github.com/sandeepthalapanane/Implementing-the-Wall-following-Algorithm-to-drive-a-robot-to-a-goal",
+    link: "",
+    image: recruitment,
+    content:
+      "Devised wall-following algorithms to autonomously guide robots through complex mazes, ensuring continuous navigation.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiCplusplus,
+        name: "C++"
+      },
+      {
+        id: "icon-2",
+        icon: SiPointy,
+        name: "Pointers"
+      },
+      {
+        id: "icon-3",
+        icon: SiDropbox,
+        name: "Doxygen"
+      },
+    ],
+  },
+  {
+    id: "project-7",
+    title: "High-speed camera mobile manipulator",
+    github: "https://github.com/sandeepthalapanane/Modeling-of-a-High-speed-camera-mobile-manipulator",
+    link: "",
+    image: cluboard,
+    content:
+      "Engineered a High-speed camera robot arm model and formulated the Kinematics & dynamics of the robot links & created a ROS package & simulated in a custom-designed Gazebo environment & visualized the camera attached at the end effector using RViz.",
+      stack: [
+        {
+          id: "icon-1",
+          icon: SiRos,
+          name: "ROS"
+        },
+        {
+          id: "icon-2",
+          icon: SiPython,
+          name: "Python"
+        },
+        {
+          id: "icon-3",
+          icon: SiGatsby,
+          name: "Gazebo"
+        },
+        {
+          id: "icon-3",
+          icon: SiGatsby,
+          name: "RViz"
+        },
+      ],
+  },
+  {
+    id: "project-8",
+    title: "Designing LQR and LQG controller for a crane system",
+    github: "https://github.com/sandeepthalapanane/LQR-and-LQG-Controller-for-a-Crane-system",
+    link: "",
+    image: cash_flow,
+    content:
+      "Derived system dynamics using Lagrangian method, implemented LQR control for linearized system, and designed LQG controller with optimal Luenberger observer for nonlinear system.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiReact,
+        name: "Matlab"
+      },
+    ],
+  },
+];
+
+export const blogPosts = [
+  {
+    id: "post-1",
+    title: "Blog Post 01 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(), // Can be edited to any string format
+    image: "https://via.placeholder.com/600/92c952",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+  {
+    id: "post-2",
+    title: "Blog Post 02 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/d32776",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+  {
+    id: "post-3",
+    title: "Blog Post 03 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/771796",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Organisations",
+    value: "2+",
+  },
+  {
+    id: "stats-2",
+    title: "Issues Opened",
+    value: "6+",
+  },
+  {
+    id: "stats-3",
+    title: "Pull Requests",
+    value: "6+",
+  },
+];
+
+export const extraCurricular = [
+  {
+    organisation: "Gamma lab - UMD",
+    title: "Driving style conditioned for autonomous vehicles",
+    duration: "Aug 2023 - Present",
+    content: [
+      {
+        text: "Designed a novel Virtual Reality driving simulator using Unity, SUMO, and RoadRunner. (Working for NeurIPS conference)",
+        link: "",
+      },
+      {
+        text: "Reduced immersive driving simulator cost by 98.8% to make it accessible for $2,500, enabling cost-effective driving research.",
+        link: "",
+      },
+      {
+        text: "Achieved enhanced path planning for AVs & and increased safety using LSTM to predict driving styles & nearby vehicle trajectories.",
+        link: "",
+      },
+      
+    ],
+    logo: nitk,
+  },
+  {
+    organisation: "Multiscale Measurements lab - UMD",
+    title: "Research Assistant",
+    duration: "Aug 2023 - Present",
+    content: [
+      {
+        text: "Executed 3D digital image correlation for stress analysis by doing 3D reconstruction using a single camera and robot arm.",
+        link: "",
+      },
+      {
+        text: "Conducted research to identify and analyze speckle patterns using a microscopic camera lens for biomechanical 3D DIC purposes.",
+        link: "",
+      },
+    ],
+    logo: nitk,
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: AiFillLinkedin,
+    link: "https://www.linkedin.com/in/sandeep-thalapanane/",
+  },
+  {
+    id: "social-media-2",
+    icon: AiFillGithub,
+    link: "https://github.com/sandeepthalapanane",
+  },
+  {
+    id: "social-media-3",
+    icon: AiFillMail,
+    link: "mailto:sandeept@umd.edu",
+  },
+  {
+    id: "social-media-5",
+    icon: AiFillInstagram,
+    link: "https://www.instagram.com/sandeep_t_/",
+  },
+];
+
+export const aboutMe = {
+    name: "Sandeep Thalapanane",
+    tagLine: "Robotics Graduate Student at University of Maryland, College Park | Graduate Research Assistant at University of Maryland ",
+    intro: "A robotics enthusiast, leveraging expertise in computer vision and machine learning to engineer groundbreaking solutions for tomorrow's autonomous systems."
+}
